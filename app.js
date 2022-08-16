1794c7f36f65378d499ea1e864fedf0e646b42bd
ashish <am5932809@gmail.com>
2022-08-16 12:48:37 +0530

    change

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const app = express();
-const dotEnv = require('dotenv').config()
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors')
-const cookieparser = require('cookie-parser')
+// const morgan = require('morgan');
+// const cors = require('cors')
+// const cookieparser = require('cookie-parser')
 const userRoute = require('./Routes/userRoute')
 const regOtp = require('./Routes/userRegotp')
 const emailVerify = require('./Routes/userEmailregotp')
 
-app.use(cors())
-app.use(morgan('dev'))
+// app.use(cors())
+// app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieparser())
+// app.use(cookieparser())
 app.use(bodyParser.json())
 
+app.use(express.json());
 
 app.use('/user', userRoute)
 app.use('/otp',regOtp)
 app.use('/email',emailVerify)
-const DB = 'mongodb+srv://nexusapi:BK8mUvspFNxlUCnG@cluster0.whfcfew.mongodb.net/nexus?retryWrites=true&w=majority'
+// const DB = 'mongodb+srv://nexusapi:BK8mUvspFNxlUCnG@cluster0.whfcfew.mongodb.net/nexus?retryWrites=true&w=majority'
 
 // mongoose.connect(process.env.MONGODB_URL_LOCAL,{
 //     useNewUrlParser: true,
@@ -31,15 +31,15 @@ const DB = 'mongodb+srv://nexusapi:BK8mUvspFNxlUCnG@cluster0.whfcfew.mongodb.net
 // .then(()=>console.log("connected"))
 // .catch((err)=>console.log(err))
 
-mongoose.connect(DB,{
-    useNewUrlParser: true,
-    useUnifiedTopology:true,
+// mongoose.connect(DB,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology:true,
    
-}).then(()=>{
-    console.log('connection successfullllll');
-}).catch((err)=>{
-    console.log(err);
-});
+// }).then(()=>{
+//     console.log('connection successfullllll');
+// }).catch((err)=>{
+//     console.log(err);
+// });
 // mongoose.connect('mongodb://localhost:27017/nexus')
 //     .then(() => {
 //         console.log("mongodb is started now")
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const http = require('http');
+require('dotenv/config');
+const mongoose = require('mongoose')
+const cors = require('cors');
+require('dotenv/config');
+
+
 const app = require('./app');
-const port = process.env.PORT || 4060;
-const server = http.createServer(app);
-server.listen(`${port}`);
+
+const port = process.env.PORT || 4020
+const DB = 'mongodb+srv://nexusapi:BK8mUvspFNxlUCnG@cluster0.whfcfew.mongodb.net/nexus?retryWrites=true&w=majority'
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+   
+}).then(()=>{
+    console.log('connection successfullllll');
+}).catch((err)=>{
+    console.log(err);
+});
+
+
+    app.use(cors());
+
+    app.listen(port,()=>{
+        console.log(`app running on port ${port}`);
+    })
