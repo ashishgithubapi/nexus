6e6786dd91bc3c57487414ffb27ffe43e8b4fbe0
ashish <am5932809@gmail.com>
2022-08-16 14:37:53 +0530

    node

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const host = '0.0.0.0';
 const port = process.env.PORT || 4060;
 const server = http.createServer(app);
-server.listen(`${port}`);
+server.listen(`${port,host}`);
